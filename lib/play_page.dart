dfe489444049a116ccbcbd54690794a26bcc8610
Philly Olowo <olowophilly11@gmail.com>
2024-11-18 09:04:35 +0300

    Changes to the main play page

--- lib/play_page.dart
+++ lib/play_page.dart
@@ -14,59 +14,109 @@ class _PlayPageState extends State<PlayPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 100,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Stack(
           children: [
-            Image.asset('assets/images/logo.png', height: 200, width: 80,),
-            const SizedBox(width: 20),
-            const Text(
-              'Power FM',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
+            Column(
+              children: [
+                Container(
+                  height: 210,
+                  child: AppBar(
+                    toolbarHeight: 100,
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/logo.png',
+                              height: 150,
+                              width: 70,
+                            ),
+                            const SizedBox(width: 20),
+                            const Text(
+                              'Power FM',
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12.0,
+                                vertical: 6.0,
+                              ),
+                            ),
+                            child: const Row(
+                              children: [
+                                Text(
+                                  "Extras",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                SizedBox(width: 2),
+                                Icon(
+                                  Icons.arrow_downward_outlined,
+                                  color: Colors.black,
+                                  size: 14, // Smaller icon size
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    backgroundColor: Colors.blue,
+                    centerTitle: true,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 70),
+                      _buildHorizontalSection1('Last Played', [
+                        _buildCard('God Only Knows', 'for KING + COUNTRY', 'assets/images/pic4.jpg'),
+                        _buildCard('To Be Announced', 'Artist Name', 'assets/images/pic1.jpg'),
+                        _buildCard('Another Song', 'Another Artist', 'assets/images/pic3.jpg'),
+                      ]),
+                      const SizedBox(height: 16),
+                      _buildHorizontalSection('New This Week', [
+                        _buildVerticalCard('Watch Live Performance', 'Seph Schlueter', 'assets/images/pic5.jpg'),
+                        _buildVerticalCard('K-LOVE Artists Performance', 'Read + Listen', 'assets/images/pic6.jpg'),
+                      ]),
+                      _buildHorizontalSection('Top Played', [
+                        _buildVerticalCard('Watch Live Performance', 'Seph Schlueter', 'assets/images/pic4.jpg'),
+                        _buildVerticalCard(
+                            'K-LOVE Artists Performance', 'Read + Listen', 'assets/images/pic5.jpg'),
+                      ]),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            // Overlapping Card
+            Positioned(
+              top: 80,
+              left: 16,
+              right: 16,
+              child: _buildPlayerSection(),
             ),
           ],
-        ),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildPlayerSection(),
-              const SizedBox(height: 16),
-              _buildHorizontalSection1('Last Played', [
-                _buildCard('God Only Knows', 'for KING + COUNTRY', 'assets/images/pic4.jpg'),
-                _buildCard('To Be Announced', 'Artist Name', 'assets/images/pic1.jpg'),
-                _buildCard('Another Song', 'Another Artist', 'assets/images/pic3.jpg'),
-              ]),
-              const SizedBox(height: 16,),
-              _buildHorizontalSection('New This Week', [
-                _buildVerticalCard('Watch Live Performance', 'Seph Schlueter', 'assets/images/pic5.jpg'),
-                _buildVerticalCard('K-LOVE Artists Performance', 'Read + Listen', 'assets/images/pic6.jpg'),
-              ]),
-
-              _buildHorizontalSection('Music All-Access', [
-                _buildVerticalCard(
-                    'Elevation Worship Praises', 'God Is not Against us', 'assets/images/pic7.jpg'),
-                _buildVerticalCard(
-                    'Elevation Worship Praises', 'God Is not Against us','assets/images/pic1.jpg'),
-              ]),
-              _buildHorizontalSection('Top Played', [
-                _buildVerticalCard('Watch Live Performance', 'Seph Schlueter', 'assets/images/pic4.jpg'),
-                _buildVerticalCard(
-                    'K-LOVE Artists Performance', 'Read + Listen', 'assets/images/pic5.jpg'),
-              ]),
-            ],
-          ),
         ),
       ),
     );
@@ -79,18 +129,25 @@ class _PlayPageState extends State<PlayPage> {
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          // Background image layer
-          Positioned.fill(
-            child: Image.asset(
-                'assets/images/pic3.jpg',
-                fit: BoxFit.cover,
-              ),
-          ),
+          // Background image with rounded corners
           Container(
             height: 200,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.4),
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: AssetImage('assets/images/pic3.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Opacity overlay container
+          Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(20),
             ),
             padding: const EdgeInsets.only(left: 8.0, right: 80.0),
             child: const Column(
@@ -102,31 +159,27 @@ class _PlayPageState extends State<PlayPage> {
               ],
             ),
           ),
+          // Floating action button
           Positioned(
-            bottom: -20,
-            right: 10,
-            child: Container(
-              height: 60,
-              width: 60,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
+            bottom: -30,
+            right: 60,
+            child: FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _audioPlayerService.playPause();
+                });
+              },
+              backgroundColor: Colors.white,
+              elevation: 2,
+              shape: const CircleBorder(
+                side: BorderSide(
                   color: Colors.blueAccent,
                   width: 2,
                 ),
               ),
-              child: FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    _audioPlayerService.playPause();
-                  });
-                },
-                backgroundColor: Colors.white,
-                elevation: 0,
-                child: Icon(
-                  _audioPlayerService.isPlaying ? Icons.pause : Icons.play_arrow,
-                  color: Colors.blueAccent,
-                ),
+              child: Icon(
+                _audioPlayerService.isPlaying ? Icons.pause : Icons.play_arrow,
+                color: Colors.blueAccent,
               ),
             ),
           ),
@@ -134,6 +187,7 @@ class _PlayPageState extends State<PlayPage> {
       ),
     );
   }
+
 
 
 
